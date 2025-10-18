de82e93cb83015b472d6a25ca2913ad9d4230dcf
nick friedrich <nfriedrich1990@gmail.com>
2025-10-18 06:03:38 +0200

    Init

---
+++ apps/dockfinity/dockfinity/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  dockfinity
+//
+//  Created by Nick Friedrich on 18.10.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ apps/dockfinity/dockfinityTests/dockfinityTests.swift
@@ -0,0 +1,17 @@
+//
+//  dockfinityTests.swift
+//  dockfinityTests
+//
+//  Created by Nick Friedrich on 18.10.25.
+//
+
+import Testing
+@testable import dockfinity
+
+struct dockfinityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
